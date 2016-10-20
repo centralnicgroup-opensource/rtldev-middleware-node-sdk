7007de72ce5d0567b2ff436fac2702a395c5e76e
Kai Schwarz <kschwarz@hexonet.net>
2016-10-20 18:09:26 +0200

    FCP3-603 session logging; added response tpl

--- defaultresponses.js
+++ defaultresponses.js
@@ -6,5 +6,6 @@
 module.exports = {
   expired: "[RESPONSE]\ncode=530\ndescription=SESSION NOT FOUND\nTRANSLATIONKEY=FAPI.530\nEOF\n",
   empty: "[RESPONSE]\ncode=423\ndescription=Empty API response\nTRANSLATIONKEY=FAPI.424\nEOF\n",
-  error: "[RESPONSE]\ncode=421\ndescription=Command failed due to server error. Client should try again\nEOF\n"
+  error: "[RESPONSE]\ncode=421\ndescription=Command failed due to server error. Client should try again\nEOF\n",
+  '404': "[RESPONSE]\ncode=421\ndescription=Page not found\nEOF\n"
 };
