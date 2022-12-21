0d49f6830375222baf7cb10d7e2f2d400192e1ab
Asif Nawaz <asif.nawaz@centralnic.com>
2022-12-21 13:02:50 +0000

    build(docker): development environment (dev container) with Node implemented

---
+++ .devcontainer/supporting_files/scripts/post-create.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# NOTE: This file will be executed as remoteUser (devcontainer.json)
+echo "=> Script: post-create.sh Executed by: $(whoami)"
+
+sudo npm install --silent --progress=false --global gulp-cli commitizen@latest cz-conventional-changelog@latest semantic-release-cli@latest
+
+# shellcheck source=/dev/null
+source ~/.zshrc
+
+# install node deps
+npm ci
+
+exit(0)
